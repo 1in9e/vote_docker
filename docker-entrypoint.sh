e141440a16ae61e4542332c6677bab87f9316d5f
ohlinge <root@ohlinge.cn>
2020-11-25 15:16:57 +0800

    update1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-echo "15 7-23/1 * * * python /vote/getVotes.py > /vote/cron_votes.log 2>&1 &\n" >> /var/spool/cron/crontabs/root
-echo "30 22 * * * python /vote/vote500.py > /vote/cron_votes_500.log 2>&1 &\n" >> /var/spool/cron/crontabs/root
-echo "10 23 * * * python /vote/vote500_1.py > /vote/cron_votes_500.log 2>&1 &\n" >> /var/spool/cron/crontabs/root
+echo "15 7-23/1 * * * python /vote/getVotes.py > /vote/cron_votes.log 2>&1 &" >> /var/spool/cron/crontabs/root
+echo "30 22 * * * python /vote/vote500.py > /vote/cron_votes_500.log 2>&1 &" >> /var/spool/cron/crontabs/root
+echo "10 23 * * * python /vote/vote500_1.py > /vote/cron_votes_500.log 2>&1 &" >> /var/spool/cron/crontabs/root
 
 crond -f
 
